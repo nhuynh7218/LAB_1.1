14be876dd8fb369fd500896f4db5f19ac7f63d05
nickn <nickn@192.168.1.2>
2017-09-28 18:44:52 -0400

    Lab

---
+++ Lab 1/src/ControlStructure.java
@@ -0,0 +1,37 @@
+public class ControlStructure 
+{
+	public static void main(String [] args)
+	{
+		fooBarBaz();
+		primePrinter();
+	}
+
+	public static void fooBarBaz() {
+		for(int x = 1; x <= 500;x++)
+		{
+			
+			if (x % 10 == 0 && x % 7 == 0 && x % 3 == 0)
+				System.out.println("FooBarBaz");
+			else if (x % 10 == 0 && x % 7 == 0)
+				System.out.println("BarBaz");
+			else if (x % 10 == 0 && x % 3== 0)
+				System.out.println("FooBaz");
+			else if (x % 7 == 0 && x % 3 == 0)
+				System.out.println("FooBar");
+			else if (x % 10 == 0)
+				System.out.println("Baz");
+			else if (x % 7 == 0)
+				System.out.println("Bar");
+			else if (x % 3 == 0)
+				System.out.println("Foo");
+			else 
+				System.out.println(x);
+		}
+		}
+	
+	public static void primePrinter()
+	{
+	}
+}
+		
+			
